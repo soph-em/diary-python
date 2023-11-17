688c1b2ea60d163209eae1594b489cce35dcac70
Sophie Earl <sophie.earl@live.com>
2023-11-17 17:15:07 +0000

    style: code cleanup

--- lib/diary_entry.py
+++ lib/diary_entry.py
@@ -21,33 +21,18 @@ class Diary_Entry():
             self.todos.append(Todo(todo.strip()))
 
     def make_contact(self):
-        # phone_regex = r"((\(?0\d{4}\)?\s?\d{3}\s?\d{3})|(\(?0\d{3}\)?\s?\d{3}\s?\d{4})|(\(?0\d{2}\)?\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$"
         phone_regex = r"(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?"
-        # phone_regex = r"\+?[1-9][0-9]{7,16}$"
         phone_numbers = re.findall(phone_regex, self.content)
 
-        # print(phone_numbers)
-        # print([number for number in re.findall(phone_regex, self.content)])
         print(re.findall(phone_regex, self.content))
         for number in phone_numbers:
-            index = self.content.index(number)
+            #splits by phone number to get name which is last index of split
             before_number = self.content.split(number)
             words_before_number = before_number[0].split(' ')
             name = words_before_number[-2]
+            
             self.contacts.append(Contact(name, number))
-            # print(self.content[index -1], number)
-            # print(type(number))
-            # print(self.content[index -2])
-            # before_number = self.content.split(number)
-            # print(before_number[0])
-            # words_before_number = before_number[0].split(' ')
-            # print(words_before_number[-2])
-            # print(self.content.split(number))
-        # phone_number_finder = r"[+]?[01423]"
-        # words_in_entry = self.content.split()
-        # for index, word in enumerate(words_in_entry):
-        #     if word.isnumeric() and re.match(phone_number_finder, word[0:1]):
-        #         self.contacts.append(Contact(words_in_entry[index -1], word))
+
 
 
 
